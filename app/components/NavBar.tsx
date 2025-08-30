d858a8de29dd1ab06ddfbfffa80bc8b57fe20409
hameelhusnain <husnainhameel3@gmail.com>
2025-08-30 23:27:54 +0500

    feat: add NavBar component for improved navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Link } from "react-router"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="navbar">
+        <Link to="/">
+        <p className='text-2xl font-bold text-gradient'>RESUMIND</p>
+        
+        </Link>
+    </nav>
   )
 }
 

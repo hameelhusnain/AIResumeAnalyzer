382f3fbb5310eef78f63e89e74a5315a08d6bbd1
hameelhusnain <husnainhameel3@gmail.com>
2025-09-16 21:35:57 +0500

    feat: refactor ResumeCard to destructure resume properties and enhance styling

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router";
 
-const ResumeCard = ({ resume }: { resume: Resume }) => {
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback} }: { resume: Resume }) => {
   return (
-    <Link to={`/resume/${resume.id}`} className="resume-card">
-      {/* Add more content here if needed */}
-      <div>{resume.companyName}</div>
-      <div>{resume.jobTitle}</div>
+    <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+      <div className="flex flex-col gap-2">
+        <h2 className="!text-black font-bold break-words">
+          {companyName}
+        </h2>
+        <h3 className="text-lg break-words text-gray-500">
+          {jobTitle}
+        </h3>
+      </div>
     </Link>
   );
 };

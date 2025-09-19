e544540bace8a138a7f67679667bab4c43e971de
hameelhusnain <husnainhameel3@gmail.com>
2025-09-20 00:31:12 +0500

    feat: add upload route with file uploader component and form for resume analysis

---
+++ app/routes/upload.tsx
@@ -0,0 +1,57 @@
+import { useState, type FormEvent } from "react"
+import NavBar from "~/components/NavBar"
+import FileUploader from "~/components/FileUploader"
+const upload = () => {
+  const [isProcessing, setProcessing] = useState(false);
+  const [statusText, setStatusText] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  
+  };
+
+  
+  return (
+ <main className="bg-[url('assets/public/images/bg-main.svg')] bg-cover">
+      <NavBar />
+
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Smart Feedback for your dream job</h1>
+          {isProcessing ? (
+            <>
+            <h2>{statusText}</h2>
+            <img src="./assets/public/images/resume-scan.gif" alt="Loading..." className="w-full"/>
+            </>
+          ) : (
+            <h2>Drop your resume for an ATS score and imporvements tips</h2>
+          )}
+          {!isProcessing && (
+            <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+             <div className="form-div">
+              <label htmlFor="company-name">Company Name</label>
+              <input type="text" name="company-name" placeholder="Company Name" id="company-name" />
+             </div>
+             <div className="form-div">
+              <label htmlFor="job-title">Job Title</label>
+              <input type="text" name="job-title" placeholder="Job Title" id="job-title" />
+             </div>
+             <div className="form-div">
+              <label htmlFor="job-description">Job Description</label>
+              <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description" />
+             </div>
+             <div className="form-div">
+              <label htmlFor="uploader">Uplaod Resume</label>
+              <FileUploader />
+             </div>
+             <button className="primary-button" type="submit">
+              Analyze Resume
+             </button>
+            </form>
+          )}
+        </div>
+      </section>
+      </main>
+  )
+}
+
+export default upload

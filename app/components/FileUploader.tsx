55fca695a35696a8f0102f40969569d0137b176e
hameelhusnain <husnainhameel3@gmail.com>
2025-09-20 00:37:58 +0500

    feat: implement file uploader component with drag-and-drop functionality

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,7 +1,22 @@
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+
 
 const FileUploader = () => {
+     const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
-    <div className="w-full gradient-border">FileUploader</div>
+    <div className="w-full gradient-border">
+          <input {...getInputProps()} />
+      <div className='space-y-4 cursor-pointer'>
+        <div className='mx-auto w-16 h-16 items-center justify-center'>
+            <img src='./assets/public/icons/info.svg' alt='upload' className='size-20' />
+        </div>
+      </div>
+    </div>
   )
 }
 
